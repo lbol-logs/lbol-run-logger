beff778826ee3d7f7b2081fdb2832d1eee722ed5
imat-cww <imat.cww@gmail.com>
2024-07-25 15:06:00 +0900

    Initial commit

---
+++ PInfo.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace RunLogger
+{
+    public static class PInfo
+    {
+        // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
+        public const string GUID = "ev.lbol.utils.runLogger";
+        public const string Name = "Run Logger";
+        public const string version = "0.0.1";
+        public static readonly Harmony harmony = new Harmony(GUID);
+
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using LBoL.Core.Dialogs;
+using System.Diagnostics;
+
+namespace RunLogger
+{
+    [HarmonyDebug]
+    [HarmonyPatch(typeof(DialogFunctions), nameof(DialogFunctions.AdventureRand))]
+    class DialogFunctions_Patch
+    {
+        static void Postfix(int a, int b, int __result)
+        {
+            BepinexPlugin.log.LogDebug($"adv rand command a:{a}, b:{b}, rez:{__result}");
+        }
+    }
+}
