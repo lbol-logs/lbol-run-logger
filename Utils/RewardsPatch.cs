1152d10575e4bb445181643dec0057fa3d22a744
imat-cww <imat.cww@gmail.com>
2024-08-22 16:57:06 +0900

    group into HandleRewards

--- Utils/RewardsPatch.cs
+++ Utils/RewardsPatch.cs
@@ -13,6 +13,16 @@ namespace RunLogger.Utils
         public static void GenerateRewards<T>(T __instance)
         {
             List<StationReward> rewards = (__instance as BattleStation).Rewards;
+            HandleRewards(rewards);
+        }
+
+        public static void AddReward(StationReward reward)
+        {
+            HandleRewards(new List<StationReward> { reward });
+        }
+
+        private static void HandleRewards(List<StationReward> rewards)
+        {
             Dictionary<string, object> Rewards = new Dictionary<string, object>();
             foreach (StationReward reward in rewards)
             {
@@ -34,35 +44,6 @@ namespace RunLogger.Utils
                     RunDataController.AddListItem2Obj(ref Rewards, Type, Exhibit);
                 }
             }
-            if (RunDataController.CurrentStation.Rewards != null)
-            {
-                Rewards = RunDataController.CurrentStation.Rewards.Concat(Rewards).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-            }
-            RunDataController.CurrentStation.Rewards = Rewards;
-        }
-
-        public static void AddReward(StationReward reward)
-        {
-            Dictionary<string, object> Rewards = new Dictionary<string, object>();
-
-            string Type = reward.Type.ToString();
-            if (Type == "Money")
-            {
-                int Money = reward.Money;
-                Rewards[Type] = Money;
-            }
-            else if (Type == "Card" || Type == "Tool")
-            {
-                List<Card> list = reward.Cards;
-                List<CardObj> Cards = RunDataController.GetCards(list);
-                RunDataController.AddListItem2Obj(ref Rewards, Type, Cards);
-            }
-            else if (Type == "Exhibit")
-            {
-                string Exhibit = reward.Exhibit.Id;
-                RunDataController.AddListItem2Obj(ref Rewards, Type, Exhibit);
-            }
-
             if (RunDataController.CurrentStation.Rewards != null)
             {
                 Rewards = RunDataController.CurrentStation.Rewards.Concat(Rewards).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
