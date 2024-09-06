b3ec73bef8b6dd174b09de28f57b2f660249011c
imat-cww <imat.cww@gmail.com>
2024-09-06 15:51:26 +0900

    remove Debugger from DialogPatch

--- Patches/DialogPatch.cs
+++ Patches/DialogPatch.cs
@@ -28,30 +28,5 @@ namespace RunLogger.Patches
                 RunDataController.AddDataItem("Values", __result);
             }
         }
-
-        [HarmonyDebug]
-        [HarmonyPatch(typeof(DialogLinePhase))]
-        class DialogLinePhasePatch
-        {
-            [HarmonyPatch(nameof(DialogLinePhase.GetLocalizedText)), HarmonyPostfix]
-            static void GetLocalizedTextPatch(string ____lineId)
-            {
-                if (!Debugger.isDebug) return;
-                Debugger.Write(____lineId);
-            }
-        }
-
-        [HarmonyDebug]
-        [HarmonyPatch(typeof(DialogOption))]
-        class DialogPatchPatch
-        {
-            [HarmonyPatch(nameof(DialogOption.GetLocalizedText)), HarmonyPostfix]
-            static void GetLocalizedTextPatch(string ____lineId, DialogOption __instance)
-            {
-                if (!Debugger.isDebug) return;
-                Debugger.Write(__instance.Id.ToString() + ": " + ____lineId);
-                if (____lineId == "line:033d1fd") AdventurePatch.BackgroundDancersPatch.HandleOptions();
-            }
-        }
     }
 }
