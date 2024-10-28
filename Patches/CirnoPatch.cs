cf5773de7aa0de1bcfbf1bf0c274bee3a4cda334
imat-cww <imat.cww@gmail.com>
2024-10-28 21:49:18 +0900

    1.3.0
    add Cirno enemy Teammates

---
+++ Patches/CirnoPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using RunLogger.Utils;
+using LBoL.EntityLib.EnemyUnits.Opponent;
+using System;
+
+namespace RunLogger.Patches
+{
+    [HarmonyPatch(typeof(Cirno))]
+    public static class CirnoPatch
+    {
+        [HarmonyPatch(nameof(Cirno.SetNextBuff)), HarmonyPrefix]
+        static void SetNextBuffPatch(Cirno __instance)
+        {
+            Type buff = __instance.NextBuff;
+            if (buff == null) return;
+
+            string id = buff.Name;
+            int level = __instance.NextLevel;
+            StatusEffectObj statusEffect = new StatusEffectObj()
+            {
+                Id = id,
+                Level = level
+            };
+            RunDataController.AddDataItem("Teammates", statusEffect);
+        }
+    }
+}
