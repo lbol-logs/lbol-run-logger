091698c49a14cfa42e1baf8eb617c003a08b39f2
imat-cww <imat.cww@gmail.com>
2024-11-18 00:15:41 +0900

    1.3.5
    refactor intention damage

--- PInfo.cs
+++ PInfo.cs
@@ -7,7 +7,7 @@ namespace RunLogger
         // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
         public const string GUID = "ev.lbol.utils.runLogger";
         public const string Name = "Run Logger";
-        public const string version = "1.3.4";
+        public const string version = "1.3.5";
         public static readonly Harmony harmony = new Harmony(GUID);
     }
 }
--- Patches/SeijaPatch.cs
+++ Patches/SeijaPatch.cs
@@ -46,7 +46,7 @@ namespace RunLogger.Patches
                 isBattleStart = false;
             }
 
-            AddIntentions(__result, __instance);
+            AddIntentions(__result);
         }
 
         private static void AddDetails(Unit unit)
@@ -94,7 +94,7 @@ namespace RunLogger.Patches
             return StatusEffects;
         }
 
-        private static void AddIntentions(IEnumerable<IEnemyMove> moves, Seija enemy)
+        private static void AddIntentions(IEnumerable<IEnemyMove> moves)
         {
             GetDetails(out TurnObj details);
             List<IntentionObj> Intentions = moves.Select((IEnemyMove m) =>
@@ -110,7 +110,7 @@ namespace RunLogger.Patches
                         {
                             AttackIntention _i = i as AttackIntention;
                             DamageInfo damageInfo = _i.Damage;
-                            int damage = enemy.Battle.CalculateDamage(enemy, enemy, enemy.Battle.Player, damageInfo);
+                            int damage = _i.CalculateDamage(damageInfo);
                             Intention = new IntentionObj()
                             {
                                 Type = type,
