3aab53bcb8743be0e22271dd0a39e0fc65e88d8c
imat-cww <imat.cww@gmail.com>
2025-04-24 21:03:26 +0900

    split `Instance` from `Controller`

--- Utils/Controller.cs
+++ Utils/Controller.cs
@@ -4,18 +4,11 @@ namespace RunLogger.Utils
 {
     internal static class Controller
     {
-        private Controller(RunLog runLog)
-        {
-            this.RunLog = runLog ?? new RunLog();
-        }
-
-        public static Controller Instance { get; private set; }
-        public bool IsTempLoaded { get; private set; }
-        public RunLog RunLog { get; private set; }
+        internal static Instance Instance { get; private set; }
 
         internal static void CreateInstance(RunLog runLog)
         {
-            Controller.Instance = new Controller(runLog);
+            Controller.Instance = new Instance(runLog);
         }
 
         internal static void DestroyInstance()
---
+++ Utils/Instance.cs
@@ -0,0 +1,14 @@
+﻿using RunLogger.Utils.RunLogLib;
+
+namespace RunLogger.Utils
+{
+    internal class Instance
+    {
+        internal Instance(RunLog runLog)
+        {
+            this.RunLog = runLog ?? new RunLog();
+        }
+
+        internal RunLog RunLog { get; private set; }
+    }
+}
