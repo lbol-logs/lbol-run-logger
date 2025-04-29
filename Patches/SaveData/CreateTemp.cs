ed155e0a29577f92235b868e01de42a9ac719852
imat-cww <imat.cww@gmail.com>
2025-04-29 11:46:45 +0900

    add `Prefix` for `CreateTemp`

--- Patches/SaveData/CreateTemp.cs
+++ Patches/SaveData/CreateTemp.cs
@@ -12,75 +12,82 @@ namespace RunLogger.Patches.SaveData
     [HarmonyPatch]
     public static class CreateTemp
     {
-        [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.Create)), HarmonyPostfix]
-        private static void StartRun(GameRunStartupParameters parameters, GameRunController __result)
+        [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.Create))]
+        public static class StartRun
         {
-            BepinexPlugin.log.LogDebug("Start run");
-            Controller.CreateInstance(new RunLog());
-
-            //InteractionViewerPatch.Listener = null;
-            //StationPatch.RewardListener = null;
-            //StationPatch.AddRewardsPatch.isAfterAddRewards = false;
-            //GameRunControllerPatch.isAfterBossReward = false;
-
-            string character = parameters.Player.Id;
-            string playerType = parameters.PlayerType.ToString().Replace("Type", "");
-            bool hasClearBonus = __result.HasClearBonus;
-            bool showRandomResult = parameters.ShowRandomResult;
-            bool isAutoSeed = __result.IsAutoSeed;
-            string difficulty = parameters.Difficulty.ToString();
-            IEnumerable<PuzzleFlag> allPuzzleFlags = PuzzleFlags.EnumerateComponents(parameters.Puzzles);
-            List<string> requests = allPuzzleFlags.Select(puzzleFlag => puzzleFlag.ToString()).ToList();
-            List<string> jadeBoxes = parameters.JadeBoxes.Select(jadeBox => jadeBox.Id).ToList();
-
-            Dictionary<string, PluginInfo> pluginInfos = BepInEx.Bootstrap.Chainloader.PluginInfos;
-            List<Mod> mods = new List<Mod>();
-            
-            foreach (string guid in Configs.PriorityMods)
+            private static void Prefix()
             {
-                if (pluginInfos.TryGetValue(guid, out PluginInfo pluginInfo)) mods.Add(ParseMod(pluginInfo));
+                BepinexPlugin.log.LogDebug("Start run");
+                Controller.CreateInstance(new RunLog());
             }
 
-            foreach ((string guid, PluginInfo pluginInfo) in pluginInfos)
+            private static void Postfix(GameRunStartupParameters parameters, GameRunController __result)
             {
-                if (Configs.PriorityMods.Contains(guid) || Configs.ExcludedMods.Contains(guid)) continue;
-                if (guid == Configs.HelpMeEirinMod) hasClearBonus = true;
-                Mod mod = ParseMod(pluginInfo);
-                if (guid == Configs.PatchouliMod) mod.Configs = PatchouliModWrapper.GetConfigs();
-                mods.Add(mod);
+
+                //InteractionViewerPatch.Listener = null;
+                //StationPatch.RewardListener = null;
+                //StationPatch.AddRewardsPatch.isAfterAddRewards = false;
+                //GameRunControllerPatch.isAfterBossReward = false;
+
+                string character = parameters.Player.Id;
+                string playerType = parameters.PlayerType.ToString().Replace("Type", "");
+                bool hasClearBonus = __result.HasClearBonus;
+                bool showRandomResult = parameters.ShowRandomResult;
+                bool isAutoSeed = __result.IsAutoSeed;
+                string difficulty = parameters.Difficulty.ToString();
+                IEnumerable<PuzzleFlag> allPuzzleFlags = PuzzleFlags.EnumerateComponents(parameters.Puzzles);
+                List<string> requests = allPuzzleFlags.Select(puzzleFlag => puzzleFlag.ToString()).ToList();
+                List<string> jadeBoxes = parameters.JadeBoxes.Select(jadeBox => jadeBox.Id).ToList();
+
+                Dictionary<string, PluginInfo> pluginInfos = BepInEx.Bootstrap.Chainloader.PluginInfos;
+                List<Mod> mods = new List<Mod>();
+
+                foreach (string guid in Configs.PriorityMods)
+                {
+                    if (pluginInfos.TryGetValue(guid, out PluginInfo pluginInfo)) mods.Add(ParseMod(pluginInfo));
+                }
+
+                foreach ((string guid, PluginInfo pluginInfo) in pluginInfos)
+                {
+                    if (Configs.PriorityMods.Contains(guid) || Configs.ExcludedMods.Contains(guid)) continue;
+                    if (guid == Configs.HelpMeEirinMod) hasClearBonus = true;
+                    Mod mod = ParseMod(pluginInfo);
+                    if (guid == Configs.PatchouliMod) mod.Configs = PatchouliModWrapper.GetConfigs();
+                    mods.Add(mod);
+                }
+
+                Controller.Instance.RunLog.Version = VersionInfo.Current.Version;
+                if (BepinexPlugin.saveProfileName.Value) Controller.Instance.RunLog.Name = parameters.UserProfile.Name;
+                Settings settings = new Settings()
+                {
+                    Character = character,
+                    PlayerType = playerType,
+                    HasClearBonus = hasClearBonus,
+                    ShowRandomResult = showRandomResult,
+                    IsAutoSeed = isAutoSeed,
+                    Difficulty = difficulty,
+                    Requests = requests,
+                    Mods = mods
+                };
+                if (jadeBoxes.Count > 0) settings.JadeBoxes = jadeBoxes;
+                Controller.Instance.RunLog.Settings = settings;
+
+                //Logger.CreateTemp();
             }
 
-            Controller.Instance.RunLog.Version = VersionInfo.Current.Version;
-            if (BepinexPlugin.saveProfileName.Value) Controller.Instance.RunLog.Name = parameters.UserProfile.Name;
-            Settings settings = new Settings()
+            private static Mod ParseMod(PluginInfo pluginInfo)
             {
-                Character = character,
-                PlayerType = playerType,
-                HasClearBonus = hasClearBonus,
-                ShowRandomResult = showRandomResult,
-                IsAutoSeed = isAutoSeed,
-                Difficulty = difficulty,
-                Requests = requests,
-                Mods = mods
-            };
-            if (jadeBoxes.Count > 0) settings.JadeBoxes = jadeBoxes;
-            Controller.Instance.RunLog.Settings = settings;
-
-            //Logger.CreateTemp();
-        }
-
-        private static Mod ParseMod(PluginInfo pluginInfo)
-        {
-            string guid = pluginInfo.Metadata.GUID;
-            string name = pluginInfo.Metadata.Name;
-            string version = pluginInfo.Metadata.Version.ToString();
-            Mod mod = new Mod()
-            {
-                GUID = guid,
-                Name = name,
-                Version = version
-            };
-            return mod;
+                string guid = pluginInfo.Metadata.GUID;
+                string name = pluginInfo.Metadata.Name;
+                string version = pluginInfo.Metadata.Version.ToString();
+                Mod mod = new Mod()
+                {
+                    GUID = guid,
+                    Name = name,
+                    Version = version
+                };
+                return mod;
+            }
         }
     }
 }
