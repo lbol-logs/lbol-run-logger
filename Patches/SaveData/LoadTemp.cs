d754b830f2e8d8aceaa4ec1e5cb5a93e799422ea
imat-cww <imat.cww@gmail.com>
2025-04-29 10:13:28 +0900

    refactor `RestorePatch`

--- Patches/SaveData/LoadTemp.cs
+++ Patches/SaveData/LoadTemp.cs
@@ -1,6 +1,21 @@
-﻿namespace RunLogger.Patches.SaveData
+﻿using HarmonyLib;
+using LBoL.Core;
+using RunLogger.Utils.RunLogLib;
+using RunLogger.Utils;
+
+namespace RunLogger.Patches.SaveData
 {
-    internal class LoadTemp
+    [HarmonyPatch]
+    public static class LoadTemp
     {
+        [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.Restore)), HarmonyPostfix]
+        private static void RestoreRun()
+        {
+            BepinexPlugin.log.LogDebug("Trying to restore run...");
+            RunLog runLog = Logger.LoadTemp();
+            if (runLog == null) return;
+            Controller.CreateInstance(runLog);
+            BepinexPlugin.log.LogDebug("Run restored");
+        }
     }
 }
