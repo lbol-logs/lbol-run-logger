f4da0f6cc1af4bcf536b8d8b66149215af4f5a9a
imat-cww <imat.cww@gmail.com>
2025-05-01 21:16:18 +0900

    split `EnemyGroupIdPatch`

--- Patches/StationObjPatches/DataPatches/EventPatches/MiyoiBartenderPatch.cs
+++ Patches/StationObjPatches/DataPatches/EventPatches/MiyoiBartenderPatch.cs
@@ -1,19 +1,20 @@
 ﻿using HarmonyLib;
-using LBoL.Core.Units;
 using LBoL.Core;
+using LBoL.EntityLib.Adventures.FirstPlace;
 using RunLogger.Utils;
 
-namespace RunLogger.Patches.StationObjPatches
+namespace RunLogger.Patches.StationObjPatches.DataPatches.EventPatches
 {
     [HarmonyPatch]
-    internal static class EnemyGroupIdPatch
+    internal static class MiyoiBartenderPatch
     {
         [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.LeaveBattle)), HarmonyPostfix]
-        private static void AddEnemyGroupId(EnemyGroup enemyGroup, GameRunController __instance)
+        private static void AddExhibitAfterBattle(GameRunController __instance)
         {
             GameRunController gameRun = __instance;
             string adventureId = Helpers.GetAdventureId(gameRun.CurrentStation);
-            if (adventureId != null) Helpers.AddDataValue("Id", enemyGroup.Id);
+            if (adventureId != nameof(MiyoiBartender)) return;
+            //MiyoiBartenderPatch.HandleBattle();
         }
     }
 }
---
+++ Patches/StationObjPatches/DataPatches/EventPatches/YachieOppressionPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using LBoL.Core;
+using LBoL.EntityLib.Adventures.Stage2;
+using RunLogger.Utils;
+
+namespace RunLogger.Patches.StationObjPatches.DataPatches.EventPatches
+{
+    [HarmonyPatch]
+    internal static class YachieOppressionPatch
+    {
+        [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.LeaveBattle)), HarmonyPostfix]
+        private static void AddExhibitAfterBattle(GameRunController __instance)
+        {
+            GameRunController gameRun = __instance;
+            string adventureId = Helpers.GetAdventureId(gameRun.CurrentStation);
+            if (adventureId != nameof(YachieOppression)) return;
+            //YachieOppressionPatch.HandleBattle();
+        }
+    }
+}
