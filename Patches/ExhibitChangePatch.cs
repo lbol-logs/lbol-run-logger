fe7f7aef6b69a6ffb3f8bdf7bfb1a436fd29d330
imat-cww <imat.cww@gmail.com>
2025-05-05 12:53:35 +0900

    minor

--- Patches/ExhibitChangePatch.cs
+++ Patches/ExhibitChangePatch.cs
@@ -9,13 +9,13 @@ namespace RunLogger.Patches
     internal static class ExhibitChangePatch
     {
         [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.GainExhibitRunner)), HarmonyPostfix, HarmonyPriority(Priority.Normal)]
-        private static void AddExhibitPatch(Exhibit exhibit)
+        private static void Add(Exhibit exhibit)
         {
             EntitiesManager.AddExhibitChange(exhibit, ChangeType.Add);
         }
 
         [HarmonyPatch(typeof(GameRunController), nameof(GameRunController.LoseExhibit)), HarmonyPostfix]
-        private static void RemoveExhibitPatch(Exhibit exhibit)
+        private static void Remove(Exhibit exhibit)
         {
             EntitiesManager.AddExhibitChange(exhibit, ChangeType.Remove);
         }
